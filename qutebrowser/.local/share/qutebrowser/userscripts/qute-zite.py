bf7f183b4d12d444174313ff0490687b7c782786
Christoffer Arvidsson <christoffer@arvidson.nu>
2025-08-21 11:34:54 +0200

    Update zite

--- qutebrowser/.local/share/qutebrowser/userscripts/qute-zite.py
+++ qutebrowser/.local/share/qutebrowser/userscripts/qute-zite.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-from os import environ as e
 import os
 import subprocess
 
-import re
-
-FIFO = open(e["QUTE_FIFO"], "w")
+FIFO = open(os.environ["QUTE_FIFO"], "w")
 
 
 def notify(message):
@@ -18,23 +15,15 @@ def notify_error(message):
 
 
 def main():
-    url = e["QUTE_URL"]
-    match = re.search(r"arxiv\.org\/[^\d]*(\d+\.\d+v?\d+)", url)
+    url = os.environ["QUTE_URL"]
+    notify(f"[zite] Fetching {url} and PDF...")
 
-    if match:
-        arxiv_id = match.group(1)
-    else:
-        notify_error("[zite] Could not extract arxiv_id")
-        exit(1)
-
-    notify(f"[zite] Fetching {arxiv_id} and PDF...")
-
-    cmd = ["zite", "fetch", arxiv_id]
+    cmd = ["zite", "fetch", url]
     result = subprocess.run(cmd)
     if result.returncode == 1:
         notify_error(f"[zite] Failed to fetch: {result.stderr}")
     else:
-        notify(f"[zite] Fetched {arxiv_id} and PDF")
+        notify(f"[zite] Fetched {url} and PDF")
 
 
 if __name__ == "__main__":
